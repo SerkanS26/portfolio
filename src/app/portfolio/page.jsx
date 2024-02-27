b05bdf0bda16cc369fe81f568bf1aa5be31f02ca
SerkanS26 <s.safran84@gmail.com>
2024-02-27 13:40:12 +0100

    About page created

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,7 +1,19 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
 
 const PortfolioPage = () => {
-  return <div>Portfolio page</div>;
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      Portfolio
+    </motion.div>
+  );
 };
 
 export default PortfolioPage;
