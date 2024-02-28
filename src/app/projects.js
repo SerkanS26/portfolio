c5258e8cb7daea4129216365f3d16fa23d09462a
SerkanS26 <s.safran84@gmail.com>
2024-02-28 23:15:12 +0100

    Portfolio and Contact Page Created

---
+++ src/app/projects.js
@@ -0,0 +1,31 @@
+const items = [
+  {
+    id: 1,
+    color: "from-red-300 to-blue-300",
+    title: "MERN Full Stack E-Commerce",
+    desc: "React, Redux Toolkit, NodeJS, Express, MongoDB, JWT, Paypal & Credit Card Payments, Admin Dashboard",
+    img: "/techshop.png",
+    link: "https://techshop.up.railway.app",
+    github: "https://github.com/SerkanS26/techshop",
+  },
+  {
+    id: 2,
+    color: "from-blue-300 to-violet-300",
+    title: "Social Media App UI",
+    desc: "React, Sass, Material UI Icons, Dark Mode, Responsive Design",
+    img: "/socialMediaUi.png",
+    link: "https://social-media-app-ui.up.railway.app/",
+    github: "https://github.com/SerkanS26/social-media-app-ui",
+  },
+  {
+    id: 3,
+    color: "from-violet-300 to-purple-300",
+    title: "Admin Dashboard UI",
+    desc: "React, Css, Material UI Icons, Recharts",
+    img: "/adminDashboardUi.png",
+    link: "https://social-media-app-ui.up.railway.app/",
+    github: "https://github.com/SerkanS26/social-media-app-ui",
+  },
+];
+
+export default items;
